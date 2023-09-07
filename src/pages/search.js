dc9274c11168f0cdb88709af6da50d25787b2261
leon3s <leo.vernisse@gmail.com>
2023-09-07 15:40:30 +0200

    refactor(pages/search): Page title duplicate next hat docs

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import Head from '@docusaurus/Head';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 function GoogleSearch() {
   return (
@@ -13,10 +12,9 @@ function GoogleSearch() {
 }
 
 export default function Search() {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={siteConfig.title}
+      title="Search"
       description="Next Hat documentation search page"
     >
       <Head>
